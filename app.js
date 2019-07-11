f54b72625f48bad9af8090e30b371700b03179f1
Sony <sonypark0204@gmail.com>
2019-07-11 15:42:04 +0900

    [feat] Applied GitHub login

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var favicon = require("serve-favicon");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
+const favicon = require("serve-favicon");
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+// const passportCongif = require('./passport');
+const GitHubStrategy = require('passport-github').Strategy;
+const githubConfig = require('./github-config');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+app.use(helmet());
+
+
+//   ----- Passport config ----
+app.use(session({
+    secret           : 'sony news!',
+    resave           : false,
+    saveUninitialized: true,
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new GitHubStrategy(githubConfig,
+    function (accessToken, refreshToken, profile, cb) {
+        // User.findOrCreate({ githubId: profile.id }, function (err, user) {
+        //     return cb(err, user);
+        // });
+        cb(null, profile);
+    }
+));
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+});
+passport.deserializeUser((user, cb) => {
+    cb(null, user)
+});
+// ------ passport config ------
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,59 +50,72 @@ app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, "public", "ico", "favicon.ico")));
 
-app.use(function(req, res, next) {
-  app.locals.pretty = true;
-  next();
+app.use(function (req, res, next) {
+    app.locals.pretty = true;
+    next();
 });
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", function(req, res) {
-  res.render("default");
+app.get("/", function (req, res) {
+    res.render("default");
 });
 
-app.get("/fluid", function(req, res) {
-  res.render("layouts/fluid");
+app.get("/fluid", function (req, res) {
+    console.log(req.user);
+    res.render("layouts/fluid");
 });
 
-app.get("/signin", function(req, res) {
-  res.render("layouts/signin");
+app.get("/signin", function (req, res) {
+    res.render("layouts/signin");
 });
 
-app.get("/signup", function(req, res) {
-  res.render("layouts/signup");
+// github login
+app.get("/github-login", passport.authenticate('github'));
+
+app.get("/auth/github/callback", passport.authenticate('github', {
+    successRedirect: '/fluid',
+    successFlash: 'Welcome!',
+    failureRedirect: '/signin',
+    failureFlash: true
+}));
+
+app.get("/signup", function (req, res) {
+    res.render("layouts/signup");
 });
 
-app.get("/sticky", function(req, res) {
-  res.render("layouts/sticky-footer");
+app.get("/sticky", function (req, res) {
+    res.render("layouts/sticky-footer");
 });
 
-app.get("/marketing-alternate", function(req, res) {
-  res.render("layouts/marketing-alternate");
+
+
+app.get("/marketing-alternate", function (req, res) {
+    res.render("layouts/marketing-alternate");
 });
 
-app.get("/marketing-narrow", function(req, res) {
-  res.render("layouts/marketing-narrow");
+app.get("/marketing-narrow", function (req, res) {
+    res.render("layouts/marketing-narrow");
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
