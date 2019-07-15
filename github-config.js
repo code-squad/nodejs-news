8520561ba166cda3a97aa434fd3e5a004f89c60c
Sony <sonypark0204@gmail.com>
2019-07-15 15:25:09 +0900

    Commite github-config file

---
+++ github-config.js
@@ -0,0 +1,7 @@
+const port = process.env.PORT || '3000';
+module.exports = {
+    clientID    : '1ac6a4fc88a4a3372720',
+    clientSecret: '1d1e56f408427c0f010dfa7641d0b8fc8bb3e541',
+    callbackURL : `http://localhost:${port}/auth/github/callback`
+};
+
