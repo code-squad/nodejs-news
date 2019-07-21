3d093c330b78a592e6d0afd931030aa54febc474
janghyoseog <bestdevhyo1225@gmail.com>
2019-07-22 01:13:53 +0900

    feat: Add focus function to email text

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,8 @@
 // email valid event 
 const emailText = document.querySelector('input[name="email"]');
+
+emailText.focus();
+
 emailText.addEventListener('keydown', () => {
     const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
     emailText.style.color = regExp.test(emailText.value) ? "forestgreen" : "crimson";
