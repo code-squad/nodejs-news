afe83d94a8370614831c536c508070d8218dca70
janghyoseog <bestdevhyo1225@gmail.com>
2019-07-22 16:21:58 +0900

    refactor: Modify config.js file

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     // TODO : Secret을 어떻게 저장할 것인지 생각할 것..!
-    'secret' : 'SeCRetKeYoFHAsInG',
+    // 'secret' : 'SeCRetKeYoFHAsInG',
     'mongodbUri' : 'mongodb://mongoadmin:hyodol@localhost:22222/news-web-app?authSource=admin'
 }
