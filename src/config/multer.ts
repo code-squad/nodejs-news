dc640b04c02dc715ae8b67b5257f40d094d6c55c
Jake <cocahack@naver.com>
2019-07-22 17:55:16 +0900

    refactor: Request 인터페이스 정의 및 multer 객체 추가
    - upload 라우터에 있던 Request 인터페이스를 이식
    - request.file의 모든 프로퍼티를 추가
    - markdown 파일을 업로드하는 multer 객체 추가

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,8 +1,33 @@
+import { Request } from 'express';
 import multer from 'multer';
 import multerS3 from 'multer-s3';
 import path from 'path';
 import { S3_BUCKET } from '../util/secrets';
 import s3 from './aws';
+
+// Default req.file is Express.Multer.File, so cannot use location and bucket property.
+export interface IS3File extends Express.Multer.File {
+  bucket               : string;
+  location             : string;
+  key                  : string;
+  fieldname            : string;
+  originalname         : string;
+  encoding             : string;
+  size                 : number;
+  acl                  : string;
+  contentType          : string;
+  contentDisposition   : boolean;
+  storageClass         : string;
+  serverSideEncryption : string;
+  metadata             : string;
+  etag                 : string;
+  level                : string;
+  message              : string;
+}
+
+export interface IS3Request extends Request {
+  file : IS3File;
+}
 
 const uploadBasePath = 'original';
 const profileImagePath = 'profile';
@@ -22,3 +47,4 @@ function multerFactory (destinationPath, limitFileSize): multer.Instance {
 }
 
 export const profileUpload: multer.Instance = multerFactory(path.join(uploadBasePath, profileImagePath), '1MB');
+export const markdownUpload: multer.Instance = multerFactory(path.join(uploadBasePath, markdownUploadPath), '1MB');
