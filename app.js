d8f67d787b85b2f928f6b7a45b30e08388bfd54e
Sony <sonypark0204@gmail.com>
2019-07-28 22:36:45 +0900

    [refactor] Delete winston
    - 아직 wiston 모듈은 추후에 공부해야겠다.
    - 아직은 다루기가 좀 까다롭다..

--- app.js
+++ app.js
@@ -2,51 +2,28 @@ require('dotenv').config();
 // require('express-async-errors'); // async middleware 대시 이걸 사용해도 된다. (이 모듈을 사용하는 게 더 간편하지만 공부 목적을 위해 직접 async middleware 를 만들어 사용했다.
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const helmet = require('helmet');
-const winston = require('winston');
 const favicon = require("serve-favicon");
-
-const methodOverride = require('method-override');
-
-const authRouter = require('./routes/auth');
-const indexRouter = require('./routes/index');
-const articlesRouter = require('./routes/articles');
-const {error404, error }= require('./middleware/error');
 const app = express();
-
 app.use(helmet());
-
-// require('./startup/routes')(app);
 
 process.on('uncaughtException', (ex) => {
     console.log('We got an uncaught exception');
-    winston.error(ex.message, ex);
+    console.error(ex.message, ex);
 });
 
-winston.configure({transports: [new winston.transports.File({ filename: 'logfile.log'})]});
 require('./startup/db')();
 require('./startup/passport')(app);
 require('./startup/flash')(app);
 
-
-// view engine setup
+// require('./startup/view')(app);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug', {defaultEngine: 'main'});
 app.use(favicon(path.join(__dirname, "public", "ico", "favicon.ico")));
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride('_method'));
 
-app.use("/", indexRouter);
-app.use('/auth', authRouter);
-app.use('/articles', articlesRouter);
-app.use(error404);
-app.use(error);
+require('./startup/routes')(app);
+
+
 
 module.exports = app;
--- startup/routes.js
+++ startup/routes.js
@@ -1,5 +1,22 @@
-
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const methodOverride = require('method-override');
+const authRouter = require('../routes/auth');
+const indexRouter = require('../routes/index');
+const articlesRouter = require('../routes/articles');
+const {error404, error }= require('../middleware/error');
 
 module.exports = (app) =>{
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({extended: false}));
+    app.use(cookieParser());
+    app.use(methodOverride('_method'));
 
-}
+    app.use("/", indexRouter);
+    app.use('/auth', authRouter);
+    app.use('/articles', articlesRouter);
+    app.use(error404);
+    app.use(error);
+};
