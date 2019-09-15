027406cb549093b2e2b58e80b2000a7f95a1f489
gminiy <gmini.y@gmail.com>
2019-09-16 07:39:47 +0900

    Step12-3 도서 등록/수정/삭제  (#41)
    * [feat] pug, semantic ui install
    
    * [feat] token 만료 에러 처리
    
    * Delete SemanticUI
    
    * [refactor] db-connect src/로 이동
    
    * [feat] login 화면 view html/css
    
    - login 하지 않은 상태에서 홈화면 이동시 로그인 화면으로 이동
    
    * [feat] 회원가입 완료
    
    * [feat] local login 구현
    
    * [feat]google login 구현 완료
    
    * [feat]logout 구현
    
    * [feat] book schema 작성
    
    * [feat] 도서 등록
    
    - router book 사용
    - POST /book/register URL 사용
    
    * README update
    
    * [refactor] jwtParser middleware를 middlewares로 변경
    
    * [feat] admin book register only
    
    * [feat] update 구현
    
    * [refactor]bookid query로 변경
    
    * [feat]delete 구현
    
    * [feat]메인 페이지
    
    * [feat]post 페이지 구성
    
    * [feat]post 등록
    
    * [feat] delete
    
    * [feat] update
    
    * [feat] 로그인 실패 시나리오
    
    * [refactor] db connect error 면 process kill
    
    * [feat] 회원가입 중 id, nickname 중복 에러 처리
    
    * [feat] 로그인 성공시 홈화면 이동
    
    * [refactor] 가독성을 위한 refactoring
    
    * [feat] pagenation
    
    * 주석 추가
    
    * README 수정

--- routes/auth/auth-controller.js
+++ routes/auth/auth-controller.js
@@ -5,37 +5,47 @@ module.exports = {
     register : async (request, response, next) => {
         try {
             const { id, password, nickname } = request.body;
-            if (!id || !password || !nickname) throw Error("id, password and nickname is required");
             const provider = 'local';
             const user = await User.create( { id, password, nickname, provider } );
             await user.save();
-            return response.redirect('/');
+            return response.send();
         } catch(error) {
+            // mongodb user model 저장 중 ID, 닉네임 중복에러 발생하면 403 Error로 중복된 요소 전송.
+            const duplicationErrorRegexp = /.*(duplicate key error).*index: (\w+)_. .*/;
+            const duplicationErrorFragment = error.message.match(duplicationErrorRegexp);
+            if(duplicationErrorFragment) {
+                const duplicateElement = duplicationErrorFragment[2];
+                return response.status(403).send(duplicateElement);
+            }
+
             next(error);
         }
     },
 
     login : async (request, response, next) => {
         try {
-            let token;
             const { id, password } = request.body;
             const user = await User.findOneById(id);
-            if (!user) throw Error("Invalided id!");
-            if(user.verify(password)) {
-                token = await jwtController.makeToken(user);
+
+            if (user && user.verify(password)) {
+                const token = await jwtController.makeToken(user);
+                response.cookie('jwt', token);
+                return response.send();
             }
-            response.cookie('jwt', token);
-            return response.json({
-                message:"login success"
-            });
+
+            return response.status(409).send("Invalid User");
         } catch(error) {
             next(error);
         }
     },
 
-    logout : (request, response) => {
-        response.clearCookie('jwt');
-        return response.status(200).redirect('/login.html');   
+    logout : (request, response, next) => {
+        try {
+            response.clearCookie('jwt');
+            return response.status(200).redirect('/login');   
+        } catch(error) {
+            next(error);
+        }
     },
 
     googleAuthenticate : (passport) => {
@@ -43,7 +53,7 @@ module.exports = {
     },
 
     googleCallbackAuthenticate : (passport) => {
-        return passport.authenticate('google', { failureRedirect: '/login.html' });
+        return passport.authenticate('google', { failureRedirect: '/login' });
     },
 
     setTokenToCookie: (request, response) => {
